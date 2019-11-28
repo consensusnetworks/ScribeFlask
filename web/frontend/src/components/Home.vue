deda4aa3ac29914c772578d24646e994395bcb43
consensuscon <csmith@consensusnetworks.com>
2019-11-28 13:20:16 -0500

    added component pieces to Vue Frontend, just need to hook them all together and connect with flask via Axios

---
+++ web/frontend/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+      <h1 class = "text-center">WELCOME></h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ web/frontend/src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <md-toolbar class="md-accent" md-elevation="1">
+      <md-menu md-direction="bottom-end">
+        <md-button class="md-icon-button" md-menu-trigger>
+            <md-icon>settings</md-icon>
+        </md-button>
+        <md-menu-content>
+            <md-menu-item>
+                <router-link
+                    :to="{ name: 'Home' }"
+                    class="button-text"
+                    >Home
+                </router-link>
+            </md-menu-item>
+            <md-menu-item>
+                <a  class="button-text" href="/logout">Logout</a>
+            </md-menu-item>
+        </md-menu-content>
+      </md-menu>
+      <md-badge md-content="1">
+          <md-button class="md-icon-button">
+              <md-icon>notifications</md-icon>
+          </md-button>
+      </md-badge>
+
+    </md-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+
+<style>
+
+</style>

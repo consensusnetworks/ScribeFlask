093d11a0ccea379fa4f461f79a85125876479b5c
consensuscon <csmith@consensusnetworks.com>
2019-12-03 11:06:33 -0500

    data is now written to table automatically

--- web/frontend/src/components/Accounttable.vue
+++ web/frontend/src/components/Accounttable.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="container">
-        <li  v-for="account in twitteraccounts" v-bind:key="account.id">
-            {{ account }}
-        </li>
+    <div id="account-table">
+        <md-table md-card md-fixed-header v-model="twitteraccounts">
+            <md-table-toolbar>
+                <h1>Tracked Twitter Accounts</h1>
+            </md-table-toolbar>
+            <md-table-row v-for="account in twitteraccounts" v-bind:key="account.id">
+                <md-table-cell md-label="Twitter Handle" md-sort-by="handle">{{account.handle}}</md-table-cell>
+                <md-table-cell md-label="Twitter ID" md-sort-by="twitterid">{{account.twitterid}}</md-table-cell>
+                <md-table-cell md-label="FactomChainid" md-sort-by="chainid">{{account.chainid}}</md-table-cell>
+                <md-table-cell md-label="Track"> 
+                            <md-button 
+                                class="md-raised button-text"
+                                > Track
+                            </md-button>
+                </md-table-cell>
+            </md-table-row> 
+        </md-table>
+    </div>
   </div>
 </template>
 
@@ -14,7 +29,7 @@
             type: Object
         }
     },
-    data () {
+    data: function() {
         return {
             twitteraccounts: [],
         };
@@ -23,6 +38,7 @@
         account: function() {
             console.log(this.account)
             this.twitteraccounts.push(this.account)
+            console.log(this.twitteraccounts)
         }
     }
   }
@@ -30,11 +46,17 @@
 </script>
 
 <style scoped>
+.button-text {
+    color: white;
+}
 .md-button {
     background-color: #f56f12;
   }
 .md-title {
     text-align: left;
+}
+.md-label {
+    color: black;
 }
 .md-table-cell {
     text-align: left;

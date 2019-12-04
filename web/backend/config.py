6aa9a497aebde89713a9360db0d598f3172bf997
consensuscon <csmith@consensusnetworks.com>
2019-12-04 13:12:30 -0500

    frontend ready to go for PoC, just adding in last API fxnality on track button

--- web/backend/config.py
+++ web/backend/config.py
@@ -9,14 +9,14 @@ class Config(object):
     TESTING = False
     KAFKA_URL = os.environ.get('KAFKA_URL', 'kafka://localhost:9092')
 
-    FC_ADDR = os.environ.get('FC_ADDR', 'FA3qdHM7pH5JDw6EaM9x5vxRtsRSy16xCEMhaJDSmECUgZQy9ob3')
-    EC_ADDR = os.environ.get('EC_ADDR', 'EC34ywXCgXE4qA3Pr52QzCfnXptKXySeYr3QcPNgqWZLyNRckgrq')
+    FC_ADDR = os.environ.get('FC_ADDR', 'YourFCTAddress')
+    EC_ADDR = os.environ.get('EC_ADDR', 'YourECAddress')
 
     RPC_USER = os.environ.get('RPC_USER', None)
     RPC_PASS = os.environ.get('RPC_PASS', None)
 
-    WALLET_BASE_URL = os.environ.get('WALLET_BASE_URL', 'http://18.222.184.135:8089')
-    FACTOM_BASE_URL = os.environ.get('FACTOM_BASE_URL', 'http://18.222.184.135:8088')
+    WALLET_BASE_URL = os.environ.get('WALLET_BASE_URL', 'http://YourHost:8089')
+    FACTOM_BASE_URL = os.environ.get('FACTOM_BASE_URL', 'http://YourHost:8088')
 
 
 

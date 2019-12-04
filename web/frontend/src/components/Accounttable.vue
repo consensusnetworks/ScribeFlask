807b774343d69e815094e073e8daeaf35b7eb154
consensuscon <csmith@consensusnetworks.com>
2019-12-03 19:42:40 -0500

    finally fixed fronend

--- web/frontend/src/components/Accounttable.vue
+++ web/frontend/src/components/Accounttable.vue
@@ -6,7 +6,7 @@
             <md-table-toolbar>
                 <h1 class="md-title">Tracked Twitter Accounts</h1>
             </md-table-toolbar>
-            <md-table-row v-for="(account, index) in twitteraccounts" :item="account.id" :key="index">
+            <md-table-row v-for="account in twitteraccounts" v-bind:key="account.id">
                 <md-table-cell md-label="Twitter Handle" md-sort-by="handle">{{account.handle}}</md-table-cell>
                 <md-table-cell md-label="Twitter ID" md-sort-by="twitterid">{{account.twitterid}}</md-table-cell>
                 <md-table-cell md-label="FactomChainid" md-sort-by="chainid">{{account.chainid}}</md-table-cell>
@@ -40,7 +40,7 @@
     watch: {
         account: function() {
             console.log(this.account)
-            this.twitteraccounts.push(this.account)
+            this.twitteraccounts = [...this.twitteraccounts, this.account]
             console.log(this.twitteraccounts)
         }
     }
